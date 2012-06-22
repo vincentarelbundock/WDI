5fd0140492b55f31b0fd7afa01de2ca65e29a95f
Vincent Arel-Bundock <varel@umich.edu>
2012-06-22 10:38:01 -0400

    multi country using good api syntax

--- test/WDI_test.R
+++ test/WDI_test.R
@@ -20,7 +20,7 @@ x = WDI(country=c('CA', 'MX', 'US'), indicator='NY.GDP.PCAP.KD', start=1960, end
 head(x)
 
 # 1 bad country (should raise warning) 
-x = WDI(country=c('BADCOUNTRY_one', 'WBADCOUNTRY_two', 'CA', 'MX', 'US'), indicator='NY.GDP.PCAP.KD', start=1960, end=2005)
+x = WDI(country=c('BADCOUNTRY_one', 'BADCOUNTRY_two', 'CA', 'MX', 'US'), indicator='NY.GDP.PCAP.KD', start=1960, end=2005)
 head(x)
 
 # Multiple indicators 1 country
@@ -43,6 +43,8 @@ x = WDIsearch('gdp.*capita')
 x = WDI(country=c('MX', 'all'), indicator=c('BADIND', x[1:4,1]))
 head(x)
 
-
+# Long list of countries
+x = WDI(country=WDI_data[[2]][,'iso2c'])
+head(x)
 
 
