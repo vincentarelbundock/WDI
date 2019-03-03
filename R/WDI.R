70fe97f66bfe2db551194ee0055b548aeff92e63
Vincent Arel-Bundock <varel@umich.edu>
2019-03-02 21:44:17 -0500

    trim white space from region

--- R/WDI.R
+++ R/WDI.R
@@ -1,3 +1,8 @@
+# https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when#comment20826625_12429344
+# 2012 hadley says "globalVariables is a hideous hack and I will never use it"
+# 2014 hadley updates his own answer with globalVariables as one of "two solutions"
+globalVariables(c('year', 'value', 'Country.Name', 'Country.Code', 'Indicator.Name', 'Indicator.Code'))
+
 #' WDI: World Development Indicators (World Bank)
 #' 
 #' Downloads the requested data by using the World Bank's API, parses the
@@ -229,6 +234,8 @@ WDIcache = function(){
     out = list('series'=series_dat, 'country'=country_dat)
     out$series = iconv(out$series, to = 'utf8')
     out$country = iconv(out$country, to = 'utf8')
+    # some regions have extra whitespace in wb data
+    out$country[, 'region'] = base::trimws(out$country[, 'region'])
     return(out)
 }
 
--- cran-comments.md
+++ cran-comments.md
@@ -1,5 +1,3 @@
 This is a minor release of WDI. No change to functionality.
 
-The UTF-8 strings in the data file are intentional and required for proper functionality.
-
 Vincent Arel-Bundock
